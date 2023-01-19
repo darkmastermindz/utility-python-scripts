2999469df8cea3ec055493d3bf9deba4bd6f7faf
Hansel Wei <hello@hanselwei.dev>
2023-01-18 23:32:58 -0500

    Refactor output
    Treat every row as new data in array of objects

--- convert_csv2json.py
+++ convert_csv2json.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import tkinter as tk
 from tkinter import filedialog
+import json
 import os
 
 def open_file():
@@ -12,16 +13,21 @@ def open_file():
         exit()
     return file_path
 
-def save_file(df):
+def save_file(data):
     json_path = filedialog.asksaveasfilename()
     if json_path[-5:] != '.json':
         json_path += '.json'
     if not json_path:
         print("Invalid file path")
         exit()
-    df.to_json(json_path)
+    with open(json_path, 'w') as f:
+        json.dump(data, f)
     print("File successfully converted and saved!")
 
+def csv_to_json(file_path):
+    df = pd.read_csv(file_path)
+    data = df.to_dict(orient='records')
+    save_file(data)
+
 file_path = open_file()
-df = pd.read_csv(file_path)
-save_file(df)
+csv_to_json(file_path)
